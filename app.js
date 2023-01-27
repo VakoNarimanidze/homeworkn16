22a96d63c0d716f0a35b362fc7b091d7621d198a
VakoNarimanidze <119787310+VakoNarimanidze@users.noreply.github.com>
2023-01-27 10:47:56 +0400

    .

---
+++ app.js
@@ -0,0 +1,56 @@
+//N1
+isEven = (x) => (x % 2 !== 0 ? true : false);
+console.log(isEven(12));
+
+//N2
+function calculateNameLength(currency) {
+  if (currency === "USD") {
+    console.log("$");
+    return "$";
+  }
+  if (currency === "Euro") {
+    console.log("€");
+    return "€";
+  }
+  if (currency === "GEL"){
+    console.log("₾");
+    return "₾";
+  };
+ 
+}
+
+//N3
+BeforeUpdate = (UpperCaseText) => UpperCaseText.toLowerCase();
+const UpdatedString = BeforeUpdate("MY NAME IS JOHN");
+console.log(UpdatedString);
+
+//N4
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const filterEvenNumbers = numbers.filter((numbers) => numbers % 2 === 0);
+console.log(filterEvenNumbers);
+
+// N5
+
+const names = ["Anna", "George"];
+const res = names.find(findAnna);
+
+names.find(findAnna);
+
+function findAnna(item) {
+  return (item = "Anna;");
+}
+console.log(res);
+
+//N6
+
+calculateExpense = (income, expense) => {
+  const distinction = income - expense;
+  return distinction;
+};
+let Jan = calculateExpense(100, 30);
+let Feb = calculateExpense(150, 80);
+let Mar = calculateExpense(200, 100);
+
+console.log(Jan);
+console.log(Feb);
+console.log(Mar);
